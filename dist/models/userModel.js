fd48bee156bf864ecffac8641b94ff3a17bf1f9e
Lazlov <Romanlazlov@gmail.com>
2025-01-27 21:59:11 +0200

    hj

--- dist/models/userModel.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-const userSchema = new Schema({
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    roles: [{ type: String, default: "user" }],
-    isVerified: { type: Boolean, default: false },
-}, { strict: true });
-const User = model("User", userSchema);
-export default User;
--- dist/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { userController } from "../controllers/userController";
-const router = express.Router();
-const { getUsers, deleteUser, updateUser, createUser } = userController;
-router.get("/", getUsers);
-router.post("/", createUser);
-router.delete("/:id", deleteUser);
-router.patch("/:id", updateUser);
-export default router;
